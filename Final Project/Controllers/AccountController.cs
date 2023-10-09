74e32eff1f75bca3dd83c471c5a71324979fd377
nguyentandat1302 <21280103e0017@student.tdmu.edu.vn>
2023-10-09 17:29:52 +0700

    LongNgu

---
+++ Final Project/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Final_Project.Controllers
+{
+    public class AccountController : Controller
+    {
+       
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult DangKy()
+        {
+
+            return View();
+        }
+        public ActionResult DangNhap()
+        {
+
+            return View();
+        }
+
+
+    }
+}
